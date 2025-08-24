dee3b3a4cb516c1720b4b19c84f603ef3ac45e7e
Amruta <amrujagtap@gmail.com>
2025-08-24 12:22:39 +0530

    Selenium project first commit

---
+++ src/main/java/pages/Login.java
@@ -0,0 +1,31 @@
+package pages;
+import base.TestBase;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebElement;
+public class Login extends TestBase{
+
+    @FindBy(id="loginusername")
+    WebElement username;
+    @FindBy(id="loginpassword")
+    WebElement password;
+    @FindBy(xpath = "//button[text()='Log in']")
+    WebElement logInBtn;
+    @FindBy(xpath = "//button[text()='Close']")
+    WebElement closeBtn;
+    //Initializing the Page Objects:
+    public Login(){
+        PageFactory.initElements(driver, this);
+    }
+
+    //Actions
+    public String validateLoginPageTitle(){
+        return driver.getTitle();
+    }
+    public ProductSelection login(String un, String pwd){
+        username.sendKeys(un);
+        password.sendKeys(pwd);
+        logInBtn.click();
+        return new ProductSelection();
+    }
+}
